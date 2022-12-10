3a5d4ef67a9255a98b4b30b80bbcb84739eb00a4
Hassan-Mehmood <hassanafridi1913@gmail.com>
2022-12-10 15:49:59 +0500

    doing some work

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
+const userDetails = {};
 async function main() {
-    const response = await inquirer.prompt({
-        message: "Guess a number. (1-10)",
-        type: "number",
-        name: "userInput",
-    });
+    console.log("Signup with your account");
+    const userResponse = await inquirer.prompt([
+        {
+            name: "accountNumber",
+            type: "number",
+            message: "Please Enter account number",
+        },
+        {
+            name: "accountPassword",
+            type: "password",
+            mask: true,
+            message: "Enter account password ",
+        },
+    ]);
+    userDetails.accountNumber = userResponse.accountNumber;
+    userDetails.accountPassword = userResponse.accountNumber;
+    console.log(userDetails);
 }
 main();
--- index.ts
+++ index.ts
@@ -1,2 +1,36 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
+
+interface userDetailsType {
+  accountNumber: number;
+  accountPassword: number;
+}
+
+const userDetails: userDetailsType = {} as userDetailsType;
+
+async function main() {
+  console.log("Signup with your account");
+
+  const userResponse = await inquirer.prompt([
+    {
+      name: "accountNumber",
+      type: "number",
+      message: "Please Enter account number",
+    },
+    {
+      name: "accountPassword",
+      type: "password",
+      mask: true,
+      message: "Enter account password ",
+    },
+  ]);
+
+  userDetails.accountNumber = userResponse.accountNumber;
+  userDetails.accountPassword = userResponse.accountNumber;
+
+  generateAccountData();
+}
+
+function generateAccountData() {}
+
+main();
